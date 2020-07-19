2ac336e3c10017faa4320e24f350fcc6773c0588
Rasmus Frederiksen <rasmus@homologation.dk>
2020-07-19 13:49:12 +0200

    added fish config as part of module instead of submodule

---
+++ .config/fish/functions/rmext.fish
@@ -0,0 +1,23 @@
+function rmext --description Remove\ an\ extension\ from\ all\ files\ in\ a\ given\ dir\ \(defaults\ to\ \'.\'\)
+    set -l finder "find"
+    set -l options (fish_opt -s d -l dir --required-val)
+    argparse $options -- $argv
+
+    # Handle finder
+    if set -q _flag_dir
+        set -a finder "$_flag_dir"
+    else
+        set -a finder "."
+    end
+
+    set -a finder "-name '*.$argv[1]' -type f"
+    #| while read NAME
+    #mv \$NAME (string trim --right --chars=$argv[1] \$NAME)
+    #end
+    set -l files (eval $finder)
+
+    for file in $files
+        echo "Removing .$argv[1] from $file"
+        mv $file (string trim --right --chars=.$argv[1] $file)
+    end
+end
