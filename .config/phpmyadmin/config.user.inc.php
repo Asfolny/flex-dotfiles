06d89552bc4508fa05fdecbb77b361bbd97842bf
Rasmus Frederiksen <rasmus@homologation.dk>
2020-09-03 10:44:58 +0200

    Removed unneeded port access to host param

--- .config/phpmyadmin/config.user.inc.php
+++ .config/phpmyadmin/config.user.inc.php
@@ -1,7 +1,7 @@
 <?php
 $i = 1;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['host'] = 'mysql8.0.17:3306';
+$cfg['Servers'][$i]['host'] = 'mysql8.0.17';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 $cfg['Servers'][$i]['port'] = 3306;
